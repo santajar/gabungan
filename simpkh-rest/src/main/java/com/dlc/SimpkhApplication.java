ffc739421517a2c49a6c65a607d7923f82d7d572
riszkhy <riszkhy@gmail.com>
2017-08-13 15:10:52 +0700

    -Modify koneksi database

--- simpkh-rest/src/main/java/com/dlc/SimpkhApplication.java
+++ simpkh-rest/src/main/java/com/dlc/SimpkhApplication.java
@@ -1,11 +1,17 @@
 package com.dlc;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimpkhApplication {
 
+	@Autowired
+	DataSource dataSource;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(SimpkhApplication.class, args);
