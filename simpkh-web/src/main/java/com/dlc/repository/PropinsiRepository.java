1595c3adf0700a332c0f5fd53d9c96c38c2170bd
Fajar Septian Dwi Saputro <fajar.saputro@internal.idn.co.id>
2017-08-20 21:50:21 +0700

    delete ALL

--- simpkh-web/src/main/java/com/dlc/repository/PropinsiRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dlc.repository;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-import id.go.kemsos.simpkh.domain.references.PropinsiEntity;
-
-
-public interface PropinsiRepository extends CrudRepository<PropinsiEntity, Integer>{}
