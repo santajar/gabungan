1252357da9749ac5f9f5cc9809d1d9d37ba462c7
riszkhy <riszkhy@gmail.com>
2017-08-30 06:04:15 +0700

    - modify export

--- simpkh-web/src/main/java/com/dlc/controller/ExportController.java
+++ simpkh-web/src/main/java/com/dlc/controller/ExportController.java
@@ -1,7 +1,11 @@
 package com.dlc.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +15,7 @@ import com.dlc.service.KabupatenServiceInterface;
 import com.dlc.service.KecamatanServiceInterface;
 import com.dlc.service.PropinsiServiceInterface;
 import com.dlc.service.UserService;
+import com.dlc.util.HibernateUtil;
 
 import id.go.kemsos.simpkh.domain.acl.User;
 import id.go.kemsos.simpkh.domain.references.KabupatenEntity;
@@ -49,4 +54,18 @@ public class ExportController {
 		modelAndView.setViewName("exportresertifikasi");
 		return modelAndView;
 	}
+	@GetMapping("/getExport")
+	public ModelAndView getExportdata(){
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		String sql = "select a.nopeserta, count(a.nopeserta) as jmlkel, a.alamat, a.tahunkepesertaan, b.nmpendamping "
+				+ "from t_kpm_resertifikasi a inner join m_pendamping b on a.kdpendamping = b.kdpendamping "
+				+ "group by nopeserta, a.alamat, a.tahunkepesertaan, nmpendamping";
+		Query query = session.createQuery(sql);
+		List<Object[]> list = query.list();
+		for(Object[] arr : list){
+			System.out.println(Arrays.toString(arr));
+		}
+		return new ModelAndView("exportresertifikasi :: resultsList");
+	}
+	
 }
