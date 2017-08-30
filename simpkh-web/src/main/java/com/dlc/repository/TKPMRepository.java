a3b0f69677de9438d90e42ffd46264a446effe69
riszkhy <riszkhy@gmail.com>
2017-08-31 04:12:09 +0700

    - add native query for tkpm

--- simpkh-web/src/main/java/com/dlc/repository/TKPMRepository.java
+++ simpkh-web/src/main/java/com/dlc/repository/TKPMRepository.java
@@ -11,6 +11,16 @@ public interface TKPMRepository extends JpaRepository<KpmResertifikasiEntity, In
 //	@Query(value ="select a.id.nomorPeserta, count(a.id.nomorPeserta) as jmlkel, a.alamat, a.tahunKepesertaan, b.nmpendamping "
 //			+ "from KpmResertifikasiEntity a inner join KpmResertifikasiEntity.pendamping b on a.kdpendamping = b.kdpendamping "
 //			+ "group by nomorPeserta, a.alamat, a.tahunKepesertaan, nmpendamping", nativeQuery = false)
-	@Query(value="select nopeserta, namaart, count(nopeserta) as jkel from t_kpm_resertifikasi group by nopeserta", nativeQuery = true)
+	@Query(value="select a.nopeserta, a.namaart,a.kab_name,"
+			+ "b.jkel,a.tahunkepesertaan,a.nmpendamping "
+			+ "from (select c.nopeserta,c.namaart,d.kab_name,c.tahunkepesertaan,e.nmpendamping "
+			+ "from t_kpm_resertifikasi c "
+			+ "inner join mst_kabupaten d "
+			+ "on c.kdkabu = d.kd_kab "
+			+ "inner join m_pendamping e "
+			+ "on c.kdpendamping = e.kdpendamping where hub_krt = '1') a "
+			+ "inner join (select nopeserta,count(nopeserta) as jkel "
+			+ "from t_kpm_resertifikasi group by nopeserta) b "
+			+ "on (a.nopeserta = b.nopeserta)", nativeQuery = true)
 	public List<Object[]> getCustomCount();
 }
